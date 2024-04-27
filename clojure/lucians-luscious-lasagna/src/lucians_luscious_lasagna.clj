1eda5d0f48a84c827c8accbe1c7b47728a7e6201
Linus Arver <linus@ucla.edu>
2024-04-27 12:08:36 -0700

    lasagna: pass tests

--- clojure/lucians-luscious-lasagna/src/lucians_luscious_lasagna.clj
+++ clojure/lucians-luscious-lasagna/src/lucians_luscious_lasagna.clj
@@ -8,23 +8,23 @@
 
 (def expected-time
   ;; Define the var here
-  )
+  40)
 
 (defn remaining-time
   "Takes the actual time in minutes the lasagna has been in the oven,
    and returns how many minutes the lasagna still has to remain in the oven."
-  [actual-time]
+  [minutes-so-far]
   ;; Define the function body here
-  )
+  (- 40 minutes-so-far))
 
 (defn prep-time
   "Takes the number of layers added to the lasagna,
    and returns how many minutes you spent preparing the lasagna"
   [num-layers]
-  )
+  (* 2 num-layers))
 
 (defn total-time
   "Takes the number of layers of lasagna and the actual time in minutes it has been in the oven.
    Returns how many minutes in total you've worked on cooking the lasagna"
-  [num-layers actual-time]
-  )
+  [num-layers minutes-so-far]
+  (+ (prep-time num-layers) minutes-so-far))
