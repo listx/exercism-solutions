6f4016eea2556fad08c5432951b4ce69ea376b69
Linus Arver <linus@ucla.edu>
2024-04-27 14:48:43 -0700

    solve cars-assemble

--- clojure/cars-assemble/src/cars_assemble.clj
+++ clojure/cars-assemble/src/cars_assemble.clj
@@ -4,9 +4,14 @@
   "Returns the assembly line's production rate per hour,
    taking into account its success rate"
   [speed]
-  )
+  (* speed 221
+     (* (cond
+          (>= speed 10) 0.77
+          (>= speed 9)  0.8
+          (>= speed 5)  0.9
+          :else         1))))
 
 (defn working-items
   "Calculates how many working cars are produced per minute"
   [speed]
-  )
+  (int (/ (production-rate speed) 60)))
