ca010a9100389523a871911ff7eb46e224547b08
Linus Arver <linus@ucla.edu>
2024-04-28 21:40:34 -0700

    tweak log-levels
    Mostly copied verbatim from
    https://exercism.org/tracks/clojure/exercises/log-levels/solutions/chicovg.

--- clojure/log-levels/src/log_levels.clj
+++ clojure/log-levels/src/log_levels.clj
@@ -1,35 +1,32 @@
 (ns log-levels
   (:require [clojure.string :as str]))
 
+(def log-line-regex
+  "The key to this regex is that it has 2 groups defined."
+  #"^\[(INFO|WARNING|ERROR)\]:\s+(.*)")
+
+(defn- parse-log-line
+  "Parse a log line into parts."
+  [s]
+  (some->> s
+           (re-find log-line-regex)
+           ;; Drop the overall match; we only want to use the groups.
+           (drop 1)))
+
 (defn message
   "Takes a string representing a log line
    and returns its message with whitespace trimmed."
   [s]
-  (let [parts (str/split s #"^\[(INFO|WARNING|ERROR)\]:\s+")]
-    (if (= 2 (count parts))
-      (str/trimr (nth parts 1))
-      s)))
-
+  (-> s parse-log-line second str/trimr))
 (defn log-level
   "Takes a string representing a log line
    and returns its level in lower-case."
   [s]
-  ; Unlike `str/split`, `re-matches` will match against the entire string. So we
-  ; need ".*" at the end here to allow matching any string as long as it has the
-  ; log level prefix.
-  ;
-  ; The leading `(?s)` is to enable dotall mode to allow running the pattern
-  ; against inputs that contain newlines.
-  (let [pat #"(?s)^\[(INFO|WARNING|ERROR)\]:\s+.*"
-        matches (re-matches pat s)]
-    (if (= 2 (count matches))
-      (-> (nth matches 1) .toLowerCase)
-      "")))
-
+  (-> s parse-log-line first str/lower-case))
 (defn reformat
   "Takes a string representing a log line and formats it
    with the message first and the log level in parentheses."
   [s]
-  (let [log-level (log-level s)
-        message (message s)]
-    (format "%s (%s)" message log-level)))
+  (format "%s (%s)"
+          (message s)
+          (log-level s)))
