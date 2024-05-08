4ff4f692ecb5b72863857caf55591439f0926019
Linus Arver <linus@ucla.edu>
2024-05-08 00:10:44 -0700

    tweak bob

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -4,15 +4,13 @@
 (defn has-words?
   "True if there are alphabetic characters in the string."
   [s]
-  (let [pat #".*[\p{IsAlphabetic}].*"
-        matches (re-matches pat s)]
-    (pos? (count matches))))
+  (re-find #"[\p{IsAlphabetic}]" s))
 
 (defn shouting?
   "True if the string is considered a shouting (all-caps) sentence."
   [s]
-  (and (= s (str/upper-case s))
-       (has-words? s)))
+  (and (has-words? s)
+       (= s (str/upper-case s))))
 
 (defn question?
   "True if the string represents a question (ends in question mark)."
@@ -21,10 +19,11 @@
       str/trimr
       (str/ends-with? "?")))
 
-(defn response-for [s] ;; <- arglist goes here
+(defn response-for [s]
   (cond
-    (= "" (str/trim s)) "Fine. Be that way!"
-    (and (shouting? s) (question? s)) "Calm down, I know what I'm doing!"
-    (shouting? s) "Whoa, chill out!"
-    (question? s) "Sure."
-    :else "Whatever."))
+    (str/blank? s)      "Fine. Be that way!"
+    (and (shouting? s)
+         (question? s)) "Calm down, I know what I'm doing!"
+    (shouting? s)       "Whoa, chill out!"
+    (question? s)       "Sure."
+    :else               "Whatever."))
