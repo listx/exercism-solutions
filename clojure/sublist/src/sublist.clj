dc277bf9c637b92674d6497a97caee8aa02c8eb5
Linus Arver <linus@ucla.edu>
2024-05-07 22:50:57 -0700

    tweak sublist
    Stolen from
    https://exercism.org/tracks/clojure/exercises/sublist/solutions/amirci,
    but with comments.
    
    Note that it's OK for `sublist?` to return `true` or `nil`, because we
    wrap it with a `cond` in `classify`. That is, if `sublist?` evaluates to
    `nil` (instead of false) it's OK because the condition will be skipped
    to the next one in `cond` because `cond` only cares about truthy
    branches. Coming from Haskell where boolean functions must return true
    or false, this feels a bit dirty but it gets the job done.

--- clojure/sublist/src/sublist.clj
+++ clojure/sublist/src/sublist.clj
@@ -1,9 +1,18 @@
 (ns sublist)
 
+(defn sublists
+  "Given a list, return a list of lists. Each item in the result
+  list is of size 'sublist-size', and is spaced apart 1 element
+  from the other lists. For example, (sublists 2 [1 2 3])
+  returns '((1 2) (2 3))."
+  [list sublist-size]
+  (partition sublist-size 1 list))
+
 (defn sublist?
   "True if list1 is a sublist of list2."
   [list1 list2]
-  (not= -1 (java.util.Collections/indexOfSubList list2 list1)))
+  (some (partial = list1)
+        (sublists list2 (count list1))))
 
 (defn classify [list1 list2]
   (cond
