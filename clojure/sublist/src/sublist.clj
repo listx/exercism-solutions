299914d52752d4a621c6e83395c1509081be1fdc
Linus Arver <linus@ucla.edu>
2024-05-07 22:28:26 -0700

    solve sublist
    This uses Java's existing standard library to do the legwork for us.
    Stolen from https://stackoverflow.com/a/15224555/437583.

--- clojure/sublist/src/sublist.clj
+++ clojure/sublist/src/sublist.clj
@@ -1,5 +1,13 @@
 (ns sublist)
 
-(defn classify [list1 list2] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn sublist?
+  "True if list1 is a sublist of list2."
+  [list1 list2]
+  (not= -1 (java.util.Collections/indexOfSubList list2 list1)))
+
+(defn classify [list1 list2]
+  (cond
+    (= list1 list2) :equal
+    (sublist? list1 list2) :sublist
+    (sublist? list2 list1) :superlist
+    :else :unequal))
