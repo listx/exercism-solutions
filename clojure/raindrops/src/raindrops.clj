894995f6cf6d3453b39b7d4647a439b80e7b8029
Linus Arver <linus@ucla.edu>
2024-05-08 00:44:44 -0700

    solve raindrops

--- clojure/raindrops/src/raindrops.clj
+++ clojure/raindrops/src/raindrops.clj
@@ -1,5 +1,22 @@
 (ns raindrops)
 
-(defn convert [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn divisible-by?
+  "Return true if n can be divided evenly (no remainder) by the divisor."
+  [divisor n]
+  (= 0 (rem n divisor)))
+
+(defn convert [n]
+  (cond
+    ((every-pred (partial divisible-by? 3)
+                 (partial divisible-by? 5)
+                 (partial divisible-by? 7)) n) "PlingPlangPlong"
+    ((every-pred (partial divisible-by? 3)
+                 (partial divisible-by? 5)) n) "PlingPlang"
+    ((every-pred (partial divisible-by? 3)
+                 (partial divisible-by? 7)) n) "PlingPlong"
+    ((every-pred (partial divisible-by? 5)
+                 (partial divisible-by? 7)) n) "PlangPlong"
+    (divisible-by? 3 n) "Pling"
+    (divisible-by? 5 n) "Plang"
+    (divisible-by? 7 n) "Plong"
+    :else (str n)))
