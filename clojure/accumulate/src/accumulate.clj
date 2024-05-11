27d8315de31ab6427ef18dd460ec4ff330561b3b
Linus Arver <linus@ucla.edu>
2024-05-11 00:35:08 -0700

    solve accumulate

--- clojure/accumulate/src/accumulate.clj
+++ clojure/accumulate/src/accumulate.clj
@@ -1,5 +1,9 @@
 (ns accumulate)
 
-(defn accumulate [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn accumulate [op coll]
+  ;; The `#(conj % (op %2))` is the function to `reduce`, which is required to
+  ;; take 2 arguments. The first argument is the accumulator (in our case, a
+  ;; vector) and the second argument is the next element to add into the vector.
+  ;; Here we call `(op %2)` to generate the next element. The `conj` simply
+  ;; appends to the accumulator.
+  (reduce #(conj % (op %2)) [] coll))
