0a9da3adf476432b870f7b497cf51c3191a6fd10
Linus Arver <linus@ucla.edu>
2024-05-10 23:28:12 -0700

    solve armstrong-numbers with a dependency
    Sadly, exercism cannot test solutions that depend on third party
    libs (for obvious security reasons). So in the next iteration we'll have
    to replace the "expt" function from numeric-tower with a custom one.

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -1,5 +1,34 @@
-(ns armstrong-numbers)
+(ns armstrong-numbers
+  ;; To get the `expt` function, do
+  ;;
+  ;;   neil dep add :lib org.clojure/math.numeric-tower :version "0.1.0"
+  ;;   clj -X:deps prep
+  ;;
+  ;; and in the CIDER repl you can do
+  ;;
+  ;;  user> (clojure.math.numeric-tower/expt 2 5)
+  ;;  ;; => 32
+  ;;
+  (:require [clojure.math.numeric-tower :as math]))
 
-(defn armstrong? [num] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn armstrong?
+  "To determine whether a number is an armstrong number, we first break it up
+  into separate digits. Then for each digit, we raise it to a power equal to the
+  total number of digits. The sum of these powers must equal the original number."
+  [num]
+  (let [
+        ;; Given a number 1025, base-10-parts is '(1025 102 10 1)...
+        base-10-parts (take-while pos? (iterate #(quot % 10) num))
+        ;; ... and num-digits would be 4 ...
+        num-digits (count base-10-parts)
+        ;; ... and digit-list would be '(5 2 0 1) ...
+        digit-list (map #(mod % 10) base-10-parts)
+        ;; ... and power-sums would be '((math/expt 5 4) (math/expt 2 4)
+        ;;                               (math/expt 0 4) (math/expt 1 4))
+        ;;
+        ;; or
+        ;;
+        ;;'(625 15 0 1)
+        power-sums (map #(math/expt % num-digits) digit-list)
+        sum (reduce + power-sums)]
+    (= num sum)))
