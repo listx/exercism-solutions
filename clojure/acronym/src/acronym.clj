17fe7aca4806dcbe5fb2079ec7dc2690c37b0fc5
Linus Arver <linus@ucla.edu>
2024-05-12 10:31:40 -0700

    solve acronym

--- clojure/acronym/src/acronym.clj
+++ clojure/acronym/src/acronym.clj
@@ -1,6 +1,21 @@
-(ns acronym)
+(ns acronym
+  (:require [clojure.string :as str]))
+
+(defn first-or-caps
+  [word]
+  (let [camel-case-caps (->> word
+                             (re-seq #"[A-Z][^A-Z]+")
+                             (map first)
+                             str/join)
+        first-letter (first word)]
+    (if (pos? (count camel-case-caps))
+      camel-case-caps
+      first-letter)))
 
 (defn acronym
   "Converts phrase to its acronym."
   [phrase]
-  )
+  (let [words (str/split phrase #"[- ]")
+        first-or-caps (map first-or-caps words)
+        result (str/upper-case (str/join first-or-caps))]
+    result))
