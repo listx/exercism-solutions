68c443bf381e143b1c15b96fd5afa2aaaba336dd
Linus Arver <linus@ucla.edu>
2024-05-13 11:14:08 -0700

    tweak anagram
    Move entire predicate logic into `anagram?` helper function.

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -4,9 +4,10 @@
 (defn anagram?
   "Return true if string `s1` is an anagram of `s2` (or vice versa)."
   [s1 s2]
-  (= (sort (into [] (str/lower-case s1)))
-     (sort (into [] (str/lower-case s2)))))
+  (and
+   (not= (str/lower-case s1) s2)
+   (= (sort (into [] (str/lower-case s1)))
+      (sort (into [] (str/lower-case s2))))))
 
 (defn anagrams-for [word prospect-list]
-  (filter #(and (not= (str/lower-case word) %)
-                (anagram? word %)) prospect-list))
+  (filter #(anagram? word %) prospect-list))
