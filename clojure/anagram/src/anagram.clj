d28eb322d43741372e4798279c295ffe16a7509b
Linus Arver <linus@ucla.edu>
2024-05-13 11:09:19 -0700

    solve anagram

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -1,5 +1,12 @@
-(ns anagram)
+(ns anagram
+  (:require [clojure.string :as str]))
 
-(defn anagrams-for [word prospect-list] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn anagram?
+  "Return true if string `s1` is an anagram of `s2` (or vice versa)."
+  [s1 s2]
+  (= (sort (into [] (str/lower-case s1)))
+     (sort (into [] (str/lower-case s2)))))
+
+(defn anagrams-for [word prospect-list]
+  (filter #(and (not= (str/lower-case word) %)
+                (anagram? word %)) prospect-list))
