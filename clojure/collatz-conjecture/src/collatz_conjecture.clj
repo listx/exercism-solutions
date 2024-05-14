6a6fcef1dc0d1b09e1274ca5de2e761d0dd0d921
Linus Arver <linus@ucla.edu>
2024-05-13 22:49:28 -0700

    solve collatz-conjecture

--- clojure/collatz-conjecture/src/collatz_conjecture.clj
+++ clojure/collatz-conjecture/src/collatz_conjecture.clj
@@ -1,5 +1,13 @@
 (ns collatz-conjecture)
 
-(defn collatz [num] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn collatz
+  [n]
+  (loop [i 0
+         x n]
+    (cond
+      (< x 1) (throw (IllegalArgumentException. "Number out of bounds."))
+      (= 1 x) i
+      :else   (recur (inc i)
+                     (if (even? x)
+                       (bit-shift-right x 1)
+                       (inc (* 3 x)))))))
