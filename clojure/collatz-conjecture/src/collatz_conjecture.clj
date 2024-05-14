f00f89eac732d3240c8921f6d69d1314b0e04467
Linus Arver <linus@ucla.edu>
2024-05-13 22:57:32 -0700

    tweak collatz-conjecture
    Prefer `(not (pos? n))` over `(< n 1)`. The former is more readable.

--- clojure/collatz-conjecture/src/collatz_conjecture.clj
+++ clojure/collatz-conjecture/src/collatz_conjecture.clj
@@ -5,9 +5,10 @@
   (loop [i 0
          x n]
     (cond
-      (< x 1) (throw (IllegalArgumentException. "Number out of bounds."))
+      (not (pos? n)) (throw (IllegalArgumentException.
+                             "Number must be positive."))
       (= 1 x) i
-      :else   (recur (inc i)
-                     (if (even? x)
-                       (bit-shift-right x 1)
-                       (inc (* 3 x)))))))
+      :else (recur (inc i)
+                   (if (even? x)
+                     (bit-shift-right x 1)
+                     (inc (* 3 x)))))))
