c1d8a952a31be6ce687040628aeee6b148cfdbf5
Linus Arver <linus@ucla.edu>
2024-05-16 01:08:26 -0700

    tweak nucleotide-count
    Based on [1] and [2].
    
    [1] https://exercism.org/tracks/clojure/exercises/nucleotide-count/solutions/amirci
    [2] https://exercism.org/tracks/clojure/exercises/nucleotide-count/solutions/jaihindhreddy

--- clojure/nucleotide-count/src/nucleotide_count.clj
+++ clojure/nucleotide-count/src/nucleotide_count.clj
@@ -1,20 +1,15 @@
-(ns nucleotide-count
-  (:require [clojure.string :refer [includes?]]))
+(ns nucleotide-count)
 
-(defn count-of-nucleotide-in-strand [nucleotide strand]
-  (when-not (includes? "ACGT" (str nucleotide))
-    (throw (IllegalArgumentException.)))
-  (->> (seq strand)
-       (filter #(= nucleotide %))
-       count))
+(def nucleotide?
+  #{\A \C \G \T})
+
+(def zero-counts (zipmap nucleotide? (repeat 0)))
 
 (defn nucleotide-counts [strand]
-  (reduce (fn [acc nucleotide]
-            (if (contains? acc nucleotide)
-              (update acc nucleotide inc)
-              acc))
-          {\A 0
-           \C 0
-           \G 0
-           \T 0}
-          (seq strand)))
+  (->> (frequencies strand)
+       (merge zero-counts)))
+
+(defn count-of-nucleotide-in-strand [nucleotide strand]
+  (when-not (nucleotide? nucleotide)
+    (throw (IllegalArgumentException.)))
+  ((nucleotide-counts strand) nucleotide))
