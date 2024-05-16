2c354442f7953082e550a43fe52d7ecb3bdaa867
Linus Arver <linus@ucla.edu>
2024-05-15 19:00:59 -0700

    tweak etl
    Copy from and document
    https://exercism.org/tracks/clojure/exercises/etl/solutions/amirci.

--- clojure/etl/src/etl.clj
+++ clojure/etl/src/etl.clj
@@ -1,13 +1,19 @@
 (ns etl
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :refer [lower-case]]))
+
+(defn unpack
+  "Unpacks a map where each entry looks like [score strs] (score is the key and
+  strs is the value). This returns a collection."
+  [[score strs]]
+  (map
+   #(vector (lower-case %) score)
+   strs))
 
 (defn transform
   "Convert a map of score-to-uppercase-letters to a map of
   lowercase-letters-to-score entries."
   [source]
-  (->> (seq source)
-       (map (fn [[score keys]]
-              (zipmap
-               (map str/lower-case keys)
-               (repeat score))))
-       (apply merge)))
+  ;; Because `unpack` returns a collection, we have to concatenate the results
+  ;; into a single collection with mapcat.
+  (->> (mapcat unpack source)
+       (into {})))
