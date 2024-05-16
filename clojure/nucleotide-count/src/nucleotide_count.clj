4fffb75831e9ea7b6e9e662737a79a9b562621af
Linus Arver <linus@ucla.edu>
2024-05-16 00:54:17 -0700

    solve nucleotide-count

--- clojure/nucleotide-count/src/nucleotide_count.clj
+++ clojure/nucleotide-count/src/nucleotide_count.clj
@@ -1,10 +1,20 @@
-(ns nucleotide-count)
+(ns nucleotide-count
+  (:require [clojure.string :refer [includes?]]))
 
-(defn count-of-nucleotide-in-strand [nucleotide strand] ;; <- Arglist goes here
-  ;; your code goes here
-)
+(defn count-of-nucleotide-in-strand [nucleotide strand]
+  (when-not (includes? "ACGT" (str nucleotide))
+    (throw (IllegalArgumentException.)))
+  (->> (seq strand)
+       (filter #(= nucleotide %))
+       count))
 
-
-(defn nucleotide-counts [strand] ;; <- Arglist goes here
-  ;; your code goes here
-)
+(defn nucleotide-counts [strand]
+  (reduce (fn [acc nucleotide]
+            (if (contains? acc nucleotide)
+              (update acc nucleotide inc)
+              acc))
+          {\A 0
+           \C 0
+           \G 0
+           \T 0}
+          (seq strand)))
