a4f35c075a672d797dfa9219c015b39f89a9e953
Linus Arver <linus@ucla.edu>
2024-05-15 22:22:00 -0700

    tweak hamming
    Just use `map` instead of `mapcat` and `vector`.

--- clojure/hamming/src/hamming.clj
+++ clojure/hamming/src/hamming.clj
@@ -2,7 +2,5 @@
 
 (defn distance [s1 s2]
   (when (= (count s1) (count s2))
-    (->> (mapcat (comp vector
-                       #(if (= % %2) 0 1))
-                 s1 s2)
+    (->> (map #(if (= % %2) 0 1) s1 s2)
          (apply +))))
