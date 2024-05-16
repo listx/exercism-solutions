6b622ca2fcefb1eb607d6177c3f38dbf62d096fa
Linus Arver <linus@ucla.edu>
2024-05-15 23:29:46 -0700

    solve nth-prime

--- clojure/nth-prime/src/nth_prime.clj
+++ clojure/nth-prime/src/nth_prime.clj
@@ -1,7 +1,14 @@
 (ns nth-prime)
 
+(def primes
+  (let [nums (iterate (partial + 2) 3)
+        factors-to-try (fn [x] (take-while #(<= (* % %) x) nums))
+        prime? (fn [x] (every? #(< 0 (mod x %)) (factors-to-try x)))]
+    (filter prime? (concat [2] nums))))
+
 (defn nth-prime 
   "Returns the prime number in the nth position."
-  [n] ;; <- arglist goes here
-  ;; your code goes here
-  )
+  [n]
+  (if (zero? n)
+    (throw (IllegalArgumentException. "There is no zeroth prime."))
+    (first (drop (- n 1) primes))))
