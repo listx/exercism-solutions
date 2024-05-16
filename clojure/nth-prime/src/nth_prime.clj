e973ebc6f45912442d27bad9abe4d592c474e780
Linus Arver <linus@ucla.edu>
2024-05-15 23:45:34 -0700

    tweak nth-prime

--- clojure/nth-prime/src/nth_prime.clj
+++ clojure/nth-prime/src/nth_prime.clj
@@ -3,12 +3,12 @@
 (def primes
   (let [nums (iterate (partial + 2) 3)
         factors-to-try (fn [x] (take-while #(<= (* % %) x) nums))
-        prime? (fn [x] (every? #(< 0 (mod x %)) (factors-to-try x)))]
+        prime? (fn [x] (every? #(pos? (mod x %)) (factors-to-try x)))]
     (filter prime? (concat [2] nums))))
 
 (defn nth-prime 
   "Returns the prime number in the nth position."
   [n]
-  (if (zero? n)
-    (throw (IllegalArgumentException. "There is no zeroth prime."))
-    (first (drop (- n 1) primes))))
+  (when-not (pos? n)
+    (throw (IllegalArgumentException.)))
+  (nth primes (dec n)))
