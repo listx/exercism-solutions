bda77e5e60ec978ef752457f7baaef208bd02ce0
Linus Arver <linus@ucla.edu>
2024-05-15 18:43:27 -0700

    solve etl

--- clojure/etl/src/etl.clj
+++ clojure/etl/src/etl.clj
@@ -1,5 +1,13 @@
-(ns etl)
+(ns etl
+  (:require [clojure.string :as str]))
 
-(defn transform [source] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn transform
+  "Convert a map of score-to-uppercase-letters to a map of
+  lowercase-letters-to-score entries."
+  [source]
+  (->> (seq source)
+       (map (fn [[score keys]]
+              (zipmap
+               (map str/lower-case keys)
+               (repeat score))))
+       (apply merge)))
