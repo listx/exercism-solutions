e2c1d1c813556da1067cb7eaba55c1839c4f7093
Linus Arver <linus@ucla.edu>
2024-05-17 00:08:02 -0700

    solve protein-translation

--- clojure/protein-translation/src/protein_translation.clj
+++ clojure/protein-translation/src/protein_translation.clj
@@ -1,9 +1,28 @@
 (ns protein-translation)
 
-(defn translate-codon [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(def codons
+  {"AUG" "Methionine"
+   "UUU" "Phenylalanine"
+   "UUC" "Phenylalanine"
+   "UUA" "Leucine"
+   "UUG" "Leucine"
+   "UCU" "Serine"
+   "UCC" "Serine"
+   "UCA" "Serine"
+   "UCG" "Serine"
+   "UAU" "Tyrosine"
+   "UAC" "Tyrosine"
+   "UGU" "Cysteine"
+   "UGC" "Cysteine"
+   "UGG" "Tryptophan"
+   "UAA" "STOP"
+   "UAG" "STOP"
+   "UGA" "STOP"})
 
-(defn translate-rna [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(def translate-codon codons)
+
+(defn translate-rna [rna]
+  (->> (partition 3 rna)
+       (map #(apply str %))
+       (map translate-codon)
+       (take-while #(not= "STOP" %))))
