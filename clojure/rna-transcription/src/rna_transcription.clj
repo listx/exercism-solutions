dccda5cddc85d84c5d51c4a0c6d10ad4a7c38779
Linus Arver <linus@ucla.edu>
2024-05-17 16:50:00 -0700

    solve rna-transcription

--- clojure/rna-transcription/src/rna_transcription.clj
+++ clojure/rna-transcription/src/rna_transcription.clj
@@ -1,5 +1,18 @@
-(ns rna-transcription)
+(ns rna-transcription
+  (:require [clojure.string :as str]))
 
-(defn to-rna [dna] ;; <- arglist goes here
-  ;; your code goes here
-)
+(def dna->rna
+  {\G \C
+   \C \G
+   \T \A
+   \A \U})
+
+(defn valid-dna? [dna]
+  (every? dna->rna dna))
+
+(defn to-rna [dna]
+  (when-not (valid-dna? dna)
+    (throw (AssertionError.)))
+  (->> (seq dna)
+       (map dna->rna)
+       (apply str)))
