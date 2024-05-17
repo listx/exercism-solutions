194032a74d3e3b3e59ef6e5cdb0386c611467b38
Linus Arver <linus@ucla.edu>
2024-05-16 23:06:33 -0700

    attempt to solve pig-latin
    The exercise is poorly written and I am not sure about rule #4. So let's
    just save this half-baked solution and see what others have done to
    encode the messy rules.

--- clojure/pig-latin/src/pig_latin.clj
+++ clojure/pig-latin/src/pig_latin.clj
@@ -1,5 +1,37 @@
-(ns pig-latin)
+(ns pig-latin
+  (:require [clojure.string :as str]))
 
-(defn translate [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn starts-with-vowel? [s]
+  (or
+    (#{\a \e \i \o \u} (first s))
+    (str/starts-with? s "xr")
+    (str/starts-with? s "yt")))
+
+(defn flipped-y [s]
+  (->> (re-find #"^([^aeiou]+)(y.*)" s)
+       (drop 1)
+       reverse
+       (apply str)))
+
+(defn flipped-leading-consonant-cluster [s]
+  (->> (re-find #"^([^aeiou]+)(.*)" s)
+       (drop 1)
+       reverse
+       (apply str)))
+
+(defn translate-word [s]
+  (let [flipped (flipped-leading-consonant-cluster s)
+        flipped-with-y (flipped-y s)]
+  (cond
+    (starts-with-vowel? s) (str s "ay")
+    flipped (if (and (str/ends-with? flipped "q") (= \u (first flipped)))
+              (str (apply str (drop 1 flipped)) \u "ay")
+              (str flipped "ay"))
+
+    (and flipped-with-y (str/includes? s "y")) (str flipped-with-y "ay")
+    :else s)))
+
+(defn translate [s]
+  (->> (str/split s #" ")
+    (map translate-word)
+    (str/join " ")))
