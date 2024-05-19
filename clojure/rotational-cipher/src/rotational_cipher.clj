6ce0d2de3e5fe0b490bbb0b9bfdc752072ca60a1
Linus Arver <linus@ucla.edu>
2024-05-18 22:50:32 -0700

    solve rotational-cipher

--- clojure/rotational-cipher/src/rotational_cipher.clj
+++ clojure/rotational-cipher/src/rotational_cipher.clj
@@ -1,5 +1,26 @@
 (ns rotational-cipher)
 
-(defn rotate [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn- lowercase? [c]
+  (<= (int \a) (int c) (int \z)))
+
+(defn- uppercase? [c]
+  (<= (int \A) (int c) (int \Z)))
+
+(defn- encodable? [c]
+  (or (lowercase? c)
+      (uppercase? c)))
+
+(defn- caesar-encode [c offset]
+  (if (encodable? c)
+    (let [start-idx (int (if (uppercase? c) \A \a))]
+      (-> (- (int c) start-idx)
+          (+ offset)
+          (mod 26)
+          (+ start-idx)
+          char))
+    c))
+
+(defn rotate
+  "Rotate letters by the given offset. Leave non-letter characters as is."
+  [plaintext offset]
+  (reduce #(str % (caesar-encode %2 offset)) "" plaintext))
