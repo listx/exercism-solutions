c90e772326ab625d9823dbdb0d7781a4aa0394a6
Linus Arver <linus@ucla.edu>
2024-05-18 23:39:51 -0700

    tweak rotational-cipher
    Adapt [1] and comment the new stuff we learned (namely, that a map can
    take 2 arguments --- the key and default value in case the key is not
    found).
    
    [1] https://exercism.org/tracks/clojure/exercises/rotational-cipher/solutions/jaihindhreddy

--- clojure/rotational-cipher/src/rotational_cipher.clj
+++ clojure/rotational-cipher/src/rotational_cipher.clj
@@ -1,26 +1,24 @@
 (ns rotational-cipher)
 
-(defn- lowercase? [c]
-  (<= (int \a) (int c) (int \z)))
+(def ^:private lowercase "abcdefghijklmnopqrstuvwxyz")
+(def ^:private uppercase "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
-(defn- uppercase? [c]
-  (<= (int \A) (int c) (int \Z)))
-
-(defn- encodable? [c]
-  (or (lowercase? c)
-      (uppercase? c)))
-
-(defn- caesar-encode [c offset]
-  (if (encodable? c)
-    (let [start-idx (int (if (uppercase? c) \A \a))]
-      (-> (- (int c) start-idx)
-          (+ offset)
-          (mod 26)
-          (+ start-idx)
-          char))
-    c))
+(defn- caesar-cipher [offset]
+  (let [lower-encoder (zipmap lowercase
+                              (drop offset (cycle lowercase)))
+        upper-encoder (zipmap uppercase
+                              (drop offset (cycle uppercase)))
+        cipher (merge lower-encoder
+                      upper-encoder)]
+    ;; Recall that in Clojure a map is a function which returns the value if the
+    ;; given key (first argument) is found. If we provide an additional (second)
+    ;; argument to the map, it acts as a default return value in case the key
+    ;; (first argument) is not found in the map. So here we basically have a map
+    ;; that returns the encoded version of a letter if the `cipher` supports it,
+    ;; but otherwise returns the given letter unchanged.
+    #(cipher % %)))
 
 (defn rotate
   "Rotate letters by the given offset. Leave non-letter characters as is."
   [plaintext offset]
-  (reduce #(str % (caesar-encode %2 offset)) "" plaintext))
+  (apply str (map (caesar-cipher offset) plaintext)))
