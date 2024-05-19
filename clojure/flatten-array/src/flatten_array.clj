d8815ea873f079963bc3699858a0f53cc29d5637
Linus Arver <linus@ucla.edu>
2024-05-19 00:18:15 -0700

    tweak flatten-array
    This solution is from [1], but with my own comments to explain how it
    works. It's surprisingly simple and uses a basic loop.
    
    [1] https://exercism.org/tracks/clojure/exercises/flatten-array/solutions/amirci

--- clojure/flatten-array/src/flatten_array.clj
+++ clojure/flatten-array/src/flatten_array.clj
@@ -1,11 +1,14 @@
 (ns flatten-array)
 
-(defn flatten
-  "Here the code is stolen from the code behind clojure.core/flatten. All of the
-  hard work is done by tree-seq, which does a depth-first walk of the given
-  collection. Each time we go down a level of nesting (when we encounter another
-  sequential collection with `sequential?`) we iterate inside it by calling
-  `seq`. Then we just remove all nils and sequential items from the walk."
-  [coll]
-  (->> (tree-seq sequential? seq coll)
-       (remove #(or (nil? %) (sequential? %)))))
+(defn flatten [arr]
+  (loop [[fst & rst :as coll] arr
+         result []]
+    (cond
+      ;; If we're done processing, return the result.
+      (empty? coll)    result
+      ;; If the element is a collection, unnest it one level and start over.
+      (coll? fst)      (recur (concat fst rst) result)
+      ;; If the element is nil, skip over it.
+      (nil? fst)       (recur rst result)
+      ;; If the element is not nil, save it to the result.
+      :else            (recur rst (conj result fst)))))
