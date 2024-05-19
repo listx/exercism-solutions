d3402d8b5953474ee7ba219a34beb00bd267abc9
Linus Arver <linus@ucla.edu>
2024-05-19 00:12:16 -0700

    tweak flatten-array
    The code is stolen from [1] which appears to have stolen it in turn from
    clojure.core/flatten.
    
    [1] https://exercism.org/tracks/clojure/exercises/flatten-array/solutions/jaihindhreddy

--- clojure/flatten-array/src/flatten_array.clj
+++ clojure/flatten-array/src/flatten_array.clj
@@ -1,5 +1,11 @@
 (ns flatten-array)
 
-(defn flatten [arr]
-  (->> (clojure.core/flatten arr)
-       (remove nil?)))
+(defn flatten
+  "Here the code is stolen from the code behind clojure.core/flatten. All of the
+  hard work is done by tree-seq, which does a depth-first walk of the given
+  collection. Each time we go down a level of nesting (when we encounter another
+  sequential collection with `sequential?`) we iterate inside it by calling
+  `seq`. Then we just remove all nils and sequential items from the walk."
+  [coll]
+  (->> (tree-seq sequential? seq coll)
+       (remove #(or (nil? %) (sequential? %)))))
