970da5f036c09d78c26237f95fd632a4012edbd8
Linus Arver <linus@ucla.edu>
2024-05-30 21:55:17 +1000

    solve run-length-encoding

--- clojure/run-length-encoding/src/run_length_encoding.clj
+++ clojure/run-length-encoding/src/run_length_encoding.clj
@@ -2,8 +2,30 @@
 
 (defn run-length-encode
   "encodes a string with run-length-encoding"
-  [plain-text])
+  [plain-text]
+  (->> (partition-by identity plain-text)
+       (reduce (fn [acc, elt]
+                 (str acc
+                      (when (< 1 (count elt)) (count elt))
+                      (first elt)))
+               "")))
+
+; See https://stackoverflow.com/a/18624940/437583 for more.
+(defn- digit? [c]
+  (and (>= 0 (compare \0 c))
+       (>= 0 (compare c \9))))
+
+(defn- expand [s]
+  (if (digit? (first s))
+    (let [[n [c]] (split-with digit? s)]
+      (->> (repeat (Integer. (apply str n)) c)
+           (apply str)))
+    s))
 
 (defn run-length-decode
   "decodes a run-length-encoded string"
-  [cipher-text])
+  [cipher-text]
+  (->> (re-seq #"[A-Za-z ]+|[0-9]+[A-Za-z ]" cipher-text)
+       (reduce (fn [acc, elt]
+                 (str acc (expand elt)))
+               "")))
