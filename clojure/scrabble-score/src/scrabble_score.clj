692b906b15beceffbd778978fc486e1b5523d3a7
Linus Arver <linus@ucla.edu>
2024-06-05 19:20:15 +1000

    solve scrabble-score

--- clojure/scrabble-score/src/scrabble_score.clj
+++ clojure/scrabble-score/src/scrabble_score.clj
@@ -1,9 +1,41 @@
-(ns scrabble-score)
+(ns scrabble-score
+  (:require [clojure.string :refer [upper-case]]))
 
-(defn score-letter [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(def scoring-rules
+  {"A" 1
+   "E" 1
+   "I" 1
+   "O" 1
+   "U" 1
+   "L" 1
+   "N" 1
+   "R" 1
+   "S" 1
+   "T" 1
+   "D" 2
+   "G" 2
+   "B" 3
+   "C" 3
+   "M" 3
+   "P" 3
+   "F" 4
+   "H" 4
+   "V" 4
+   "W" 4
+   "Y" 4
+   "K" 5
+   "J" 8
+   "X" 8
+   "Q" 10
+   "Z" 10})
 
-(defn score-word [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn score-letter [ltr]
+  (->> ltr
+       upper-case
+       scoring-rules))
+
+(defn score-word [ltrs]
+  (->> ltrs
+       char-array
+       (map score-letter)
+       (apply +)))
