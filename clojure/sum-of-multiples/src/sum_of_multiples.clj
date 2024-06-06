04af997068589b1db115d1ac360a6d03089a4166
Linus Arver <linus@ucla.edu>
2024-06-06 17:52:33 +1000

    tweak sum-of-multiples
    See [1].
    
    [1] https://exercism.org/tracks/clojure/exercises/sum-of-multiples/solutions/amirci

--- clojure/sum-of-multiples/src/sum_of_multiples.clj
+++ clojure/sum-of-multiples/src/sum_of_multiples.clj
@@ -1,14 +1,7 @@
 (ns sum-of-multiples)
 
-(defn multiples-of [n]
-  (map #(* n %) (iterate inc 1)))
-
 (defn sum-of-multiples [base-vals level]
   (->> base-vals
-       (map multiples-of)
-       (map (fn [multiples]
-              (take-while #(< % level)
-                          multiples)))
-       flatten
+       (mapcat #(range % level %))
        set
        (apply +)))
