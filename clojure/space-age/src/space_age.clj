4c9156c32a3e7458f3ac57a1a6ac80bd98809612
Linus Arver <linus@ucla.edu>
2024-06-06 17:09:43 +1000

    solve space-age

--- clojure/space-age/src/space_age.clj
+++ clojure/space-age/src/space_age.clj
@@ -1,33 +1,42 @@
 (ns space-age)
 
-(defn on-mercury [] ;; <- arglist goes here
-  ;; your code goes here
-)
+;; Mercury: orbital period 0.2408467 Earth years
+;; Venus: orbital period 0.61519726 Earth years
+;; Earth: orbital period 1.0 Earth years, 365.25 Earth days, or 31557600 seconds
+;; Mars: orbital period 1.8808158 Earth years
+;; Jupiter: orbital period 11.862615 Earth years
+;; Saturn: orbital period 29.447498 Earth years
+;; Uranus: orbital period 84.016846 Earth years
+;; Neptune: orbital period 164.79132 Earth years
 
-(defn on-venus [] ;; <- arglist goes here
-  ;; your code goes here
-)
 
-(defn on-earth [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn on-earth [secs]
+  (/ secs 31557600.0))
 
-(defn on-mars [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn on-mercury [secs]
+  (-> (on-earth secs)
+      (/ 0.2408467)))
 
-(defn on-jupiter [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn on-venus [secs]
+  (-> (on-earth secs)
+      (/ 0.61519726)))
 
-(defn on-saturn [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn on-mars [secs]
+  (-> (on-earth secs)
+      (/ 1.8808158)))
 
-(defn on-uranus [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn on-jupiter [secs]
+  (-> (on-earth secs)
+      (/ 11.862615)))
 
-(defn on-neptune [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn on-saturn [secs]
+  (-> (on-earth secs)
+      (/ 29.447498)))
+
+(defn on-uranus [secs]
+  (-> (on-earth secs)
+      (/ 84.016846)))
+
+(defn on-neptune [secs]
+  (-> (on-earth secs)
+      (/ 164.79132)))
