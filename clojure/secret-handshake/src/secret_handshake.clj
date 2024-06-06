6079ff8a52c3778b08cb2e42aa67c72d7f4a77a9
Linus Arver <linus@ucla.edu>
2024-06-06 16:39:54 +1000

    tweak secret-handshake
    Comment the solution from [1].
    
    [1] https://exercism.org/tracks/clojure/exercises/secret-handshake/solutions/amirci

--- clojure/secret-handshake/src/secret_handshake.clj
+++ clojure/secret-handshake/src/secret_handshake.clj
@@ -1,5 +1,19 @@
 (ns secret-handshake)
 
-(defn commands [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn commands [nbr]
+  (->> ["wink" "double blink" "close your eyes" "jump" reverse]
+       ;; map-indexed is like Python's `enumerate`.
+       (map-indexed vector)
+       (reduce
+        (fn [acc [bit x]]
+          ;; Create a function which can either append into a collection, or be
+          ;; itself. In our case, the `reverse` item will be itself, while the
+          ;; string elements will all become "appenders", so to speak.
+          (let [f (if (string? x)
+                    #(conj % x)
+                    x)]
+            (cond-> acc
+              ;; Thankfully, bit-test uses 0-based indexing, so we can use the
+              ;; indices from `map-indexed` (which start at 0) directly.
+              (bit-test nbr bit) f)))
+        [])))
