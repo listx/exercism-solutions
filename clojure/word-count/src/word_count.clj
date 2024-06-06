dc32ffb1fa6f9d40d4897dc8a1a437de9c6c54d2
Linus Arver <linus@ucla.edu>
2024-06-06 18:07:22 +1000

    solve word-count

--- clojure/word-count/src/word_count.clj
+++ clojure/word-count/src/word_count.clj
@@ -1,5 +1,9 @@
-(ns word-count)
+(ns word-count
+  (:require [clojure.string :refer [lower-case]]))
 
-(defn word-count [s] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn word-count [s]
+  (->> s
+       (re-seq #"[A-Za-z0-9]([A-Za-z0-9']+)?")
+       (map first)
+       (map lower-case)
+       frequencies))
