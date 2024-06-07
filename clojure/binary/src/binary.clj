7a8422292ce0ed63a375df6fd46552e1b050f2fd
Linus Arver <linus@ucla.edu>
2024-06-07 14:09:58 +1000

    solve binary

--- clojure/binary/src/binary.clj
+++ clojure/binary/src/binary.clj
@@ -1,5 +1,15 @@
 (ns binary)
 
-(defn to-decimal [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn binary? [s]
+  (re-matches #"^[01]+$" s))
+
+(defn to-decimal [s]
+  (if (binary? s)
+    (->> s
+         (map {\0 0
+               \1 1})
+         reverse
+         (map vector (iterate (partial * 2) 1))
+         (map (partial apply *))
+         (apply +))
+    0))
