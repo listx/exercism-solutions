910d237a5f0ef55a2b5e6a7ce43c4b10f2f00315
Linus Arver <linus@ucla.edu>
2024-06-07 17:26:31 +1000

    comment binary-search-tree
    The main interesting things are:
    
    - The use of `declare` to get around circular dependency problems (where
      one function depends on another, which also depends on the first).
    
    - The use of a nested hashmap to represent the BST.
    
    - The use of `:foo` as a lookup function (to look up the key `:foo` in a
      hashmap).
    
    - The `{:keys [...]}` deconstructing trick to quickly bind values with
      the corresponding keys.
    
    - The use of `update` to update an arbitrarily nested element, by
      calling a helper function which can in turn invoke the enclosing
      function (in this case `insert`) to reach into the structure.
    
    - The use of recursion in `to-list` to flatten the nested BST structure.

--- clojure/binary-search-tree/src/binary_search_tree.clj
+++ clojure/binary-search-tree/src/binary_search_tree.clj
@@ -4,23 +4,40 @@
 
 (defn singleton [n] {:value n})
 
+;; Forward-declare `insert` so that we can use it in `singleton-or-follow`. If
+;; we don't use a forward declaration we would be stuck (forever) trying to
+;; unwrangle a circular dependency.
 (declare insert)
 
+;; Insert a value into an existing tree, or create a new tree. The "existing
+;; tree" checks if `bst` is a node. A node must have a `:value` key, so that's
+;; the check.
 (defn singleton-or-follow [bst n]
-  (if (value bst)
+  (if (:value bst)
     (insert n bst)
     (singleton n)))
 
+;; The {:keys [...]} is discussed in https://clojure.org/guides/destructuring.
+;; But basically given `{:keys [a b c]}` we bind the values for the keys :a, :b,
+;; and :c to `a`, `b` and `c`.
 (defn insert [n {:keys [value] :as bst}]
-  (cond
-    (< n value) (update bst :left singleton-or-follow n)
-    (< value n) (update bst :right singleton-or-follow n)
-    :else {:value n :left bst}))
+  (case (compare n value)
+    ;; Grow the hashmap by creating a new root node with the same value.
+    0  {:value n :left bst}
+    ;; Descend one level by going into the value of for the key :left. If it is
+    ;; another non-leaf node, it will have a `:value` key. The call to
+    ;; `single-or-follow` ends up as `(single-or-follow (:left bst) n)` because
+    ;; the old value is supplied as the first argument.
+    -1 (update bst :left  singleton-or-follow n)
+    ;; Same as above, but for the `:right` key.
+    1  (update bst :right singleton-or-follow n)))
 
 (def left :left)
 
 (def right :right)
 
+;; Recurse into the `bst` structure, but keep the current node's `value` in the
+;; middle.
 (defn to-list [{:keys [value left right] :as bst}]
   (if-not bst
     []
@@ -30,5 +47,7 @@
 (defn from-list [[fst & rst]]
   (reduce
    #(insert %2 %1)
+   ;; Create initial bst from the first element.
    (singleton fst)
+   ;; Process remaining elements.
    rst))
