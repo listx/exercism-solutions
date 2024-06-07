306a78adda628e0a900a2baf8659ceb6471fef34
Linus Arver <linus@ucla.edu>
2024-06-07 16:19:16 +1000

    solve binary-search
    This is just a reworded version of [1]. The key thing we were missing
    was an ability to remember an index offset across recursive calls.
    
    [1] https://exercism.org/tracks/clojure/exercises/binary-search/solutions/amirci

--- clojure/binary-search/src/binary_search.clj
+++ clojure/binary-search/src/binary_search.clj
@@ -1,26 +1,17 @@
 (ns binary-search)
 
-(defn middle
-  "Return the middle item from coll, as well as its index"
-  [coll]
-  (let [len (count coll)
-        mid (quot len 2)]
-      [(nth coll mid) mid]))
-
-(defn search-for-helper [item mid-idx coll]
-  (let [mid-item (nth coll mid-idx)]
-    (cond
-      (= mid-item item) mid-idx
-
-      (or (zero? mid-idx) (> mid-idx (dec (count coll))))
-        (throw (IllegalArgumentException. "Number not found."))
-
-      (< item mid-item) (search-for-helper item (quot mid-idx 2) coll)
-
-      (< mid-item item) (search-for-helper item (+ 1 mid-idx (quot (- (count coll) mid-idx) 2)) coll))))
+(defn middle-idx [haystack]
+  (quot (count haystack) 2))
 
 (defn search-for
-  "Return the index of the item from the collection."
-  [item coll]
-  (let [[_ mid-idx] (middle coll)]
-    (search-for-helper item mid-idx coll)))
+  ([needle haystack] (search-for needle haystack 0))
+  ([needle haystack offset]
+   (let [midx (middle-idx haystack)
+         [lft [candidate & rgt]] (split-at midx haystack)]
+
+     (assert (seq haystack) (str "Element " needle " not found"))
+
+     (case (compare needle candidate)
+       0  (+ offset midx)
+       -1 (recur needle lft offset)
+       1  (recur needle rgt (+ offset midx 1))))))
