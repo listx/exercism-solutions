94599c88609908e8314ef54038ea174fa9755726
Linus Arver <linus@ucla.edu>
2024-06-07 14:13:09 +1000

    tweak binary
    Although the use of `re-matches` is more precise, we can get away with
    this simplification.

--- clojure/binary/src/binary.clj
+++ clojure/binary/src/binary.clj
@@ -1,15 +1,10 @@
 (ns binary)
 
-(defn binary? [s]
-  (re-matches #"^[01]+$" s))
-
 (defn to-decimal [s]
-  (if (binary? s)
-    (->> s
-         (map {\0 0
-               \1 1})
-         reverse
-         (map vector (iterate (partial * 2) 1))
-         (map (partial apply *))
-         (apply +))
-    0))
+  (->> s
+       (map #({\0 0
+               \1 1} % 0))
+       reverse
+       (map vector (iterate (partial * 2) 1))
+       (map (partial apply *))
+       (apply +)))
