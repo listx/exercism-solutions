3d68aca0660ef2d9520540fb0f3222118e30f9ee
Linus Arver <linus@ucla.edu>
2024-06-07 13:45:46 +1000

    tweak beer-song
    Use `condp` instead of `cond`.

--- clojure/beer-song/src/beer_song.clj
+++ clojure/beer-song/src/beer_song.clj
@@ -4,16 +4,15 @@
 (defn verse
   "Returns the nth verse of the song."
   [n]
-
-  (cond
-    (= 0 n) (str         "No more bottles of beer on the wall, no more bottles of beer.\n"
-                         "Go to the store and buy some more, 99 bottles of beer on the wall.\n")
-    (= 1 n) (str (format "%d bottle of beer on the wall, %d bottle of beer.\n" n n)
-                         "Take it down and pass it around, no more bottles of beer on the wall.\n")
-    (= 2 n) (str (format "%d bottles of beer on the wall, %d bottles of beer.\n" n n)
-                         "Take one down and pass it around, 1 bottle of beer on the wall.\n")
-    :else   (str (format "%d bottles of beer on the wall, %d bottles of beer.\n" n n)
-                 (format "Take one down and pass it around, %d bottles of beer on the wall.\n" (dec n)))))
+  (condp = n
+    0 (str         "No more bottles of beer on the wall, no more bottles of beer.\n"
+                   "Go to the store and buy some more, 99 bottles of beer on the wall.\n")
+    1 (str (format "%d bottle of beer on the wall, %d bottle of beer.\n" n n)
+                   "Take it down and pass it around, no more bottles of beer on the wall.\n")
+    2 (str (format "%d bottles of beer on the wall, %d bottles of beer.\n" n n)
+                   "Take one down and pass it around, 1 bottle of beer on the wall.\n")
+    (str (format "%d bottles of beer on the wall, %d bottles of beer.\n" n n)
+          (format "Take one down and pass it around, %d bottles of beer on the wall.\n" (dec n)))))
 
 (defn sing
   "Given a start and an optional end, returns all verses in this interval. If
