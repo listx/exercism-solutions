856eed2a4c94e71c0ab3095f3be37ddeb30b7337
Linus Arver <linus@ucla.edu>
2024-06-07 18:19:00 +1000

    attempt to solve "change"
    This is not an easy problem. See [1] for a variation of this problem.
    
    We should probbly just treat it as a tree problem. But I'm too lazy for
    that now.
    
    [1] http://funloop.org/codex/problem/making_change/README.html

--- clojure/change/src/change.clj
+++ clojure/change/src/change.clj
@@ -1,5 +1,24 @@
 (ns change)
 
-(defn issue [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn largest-fitting-coin [sum coins]
+  (->> coins
+       (into '())
+       sort
+       reverse
+       (drop-while #(< sum %))
+       first))
+
+(defn issue [sum coins]
+  (when (neg? sum)
+    (throw (IllegalArgumentException. "cannot change")))
+  (when (< sum (->> coins (into '()) sort first))
+    (throw (IllegalArgumentException. "cannot change")))
+
+  (loop [remaining sum
+         coin (largest-fitting-coin sum coins)
+         result ()]
+    (if (zero? remaining)
+      result
+      (recur (- remaining coin)
+             (largest-fitting-coin (- remaining coin) coins)
+             (cons coin result)))))
