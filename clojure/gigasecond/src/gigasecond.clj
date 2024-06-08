2f53a8e87f5f3f167687352d554b06c660deb418
Linus Arver <linus@ucla.edu>
2024-06-08 22:25:52 +1000

    copy solution for gigasecond
    See [1].
    
    [1] https://exercism.org/tracks/clojure/exercises/gigasecond/solutions/amirci

--- clojure/gigasecond/src/gigasecond.clj
+++ clojure/gigasecond/src/gigasecond.clj
@@ -1,5 +1,24 @@
-(ns gigasecond)
+(ns gigasecond
+  (:import [java.time LocalDateTime]))
 
-(defn from []  ;; <- arglist goes here
-    ;; your code goes here
-)
+(def extract-ymd
+  (juxt
+   #(.getYear %)
+   #(.getMonthValue %)
+   #(.getDayOfMonth %)))
+
+(defn datetime-from [y m d]
+  ;; See
+  ;; https://docs.oracle.com/javase%2F8%2Fdocs%2Fapi%2F%2F/java/time/LocalDateTime.html#of-int-int-int-int-int-int-.
+  (LocalDateTime/of y m d 0 0 0))
+
+(defn from
+  "Add 1 billion seconds from the given date."
+  [y m d]
+  (-> ;; Construct LocalDateTime object.
+      (datetime-from y m d)
+      ;; Add 1 billion seconds.
+      (.plusSeconds (Math/pow 10 9))
+      ;; Retrieve the LocalDate part of the (modified) LocalDateTime.
+      (.toLocalDate)
+      extract-ymd))
