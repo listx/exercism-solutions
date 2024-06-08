a76c650aa56dda43dc402c1731df125ae1f428a3
Linus Arver <linus@ucla.edu>
2024-06-08 22:44:28 +1000

    copy solution for grade-school
    See https://exercism.org/tracks/clojure/exercises/grade-school/solutions/amirci.
    
    Add comments.

--- clojure/grade-school/src/grade_school.clj
+++ clojure/grade-school/src/grade_school.clj
@@ -1,13 +1,19 @@
 (ns grade-school)
 
-(defn grade [school grade]  ;; <- arglist goes here
-    ;; your code goes here
-)
+(defn grade
+  [db gr]
+  ;; Return the empty vector is a non-existent key (grade) is fetched.
+  (get db gr []))
 
-(defn add [school name grade]  ;; <- arglist goes here
-    ;; your code goes here
-)
+(defn add
+  [db name gr]
+  ;; Return a new db (hashmap) with the `name` appended to the old value.
+  (update db gr concat [name]))
 
-(defn sorted [school]  ;; <- arglist goes here
-    ;; your code goes here
-)
+(defn sorted
+  [db]
+  ;; `sorted-map` sorts a hashmap by its keys. Because the value are left as-is,
+  ;; we have to call `(sort v)`.
+  (into (sorted-map)
+    ;; Note that [[k v] db] can range through all keys and values in `db`.
+    (for [[k v] db] [k (sort v)])))
