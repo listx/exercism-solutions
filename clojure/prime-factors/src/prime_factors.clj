45fe79627402b480bb7db91366eb60ebd5d4154a
Linus Arver <linus@ucla.edu>
2024-06-09 22:00:12 +1000

    solve prime-factors

--- clojure/prime-factors/src/prime_factors.clj
+++ clojure/prime-factors/src/prime_factors.clj
@@ -1,5 +1,14 @@
 (ns prime-factors)
 
-(defn of [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn of [n]
+  (loop [[factor & rst :as factors] (iterate inc 2)
+         remaining n
+         result []]
+    (cond
+      (= 1 remaining) result
+      (zero? (rem remaining factor)) (recur factors
+                                            (quot remaining factor)
+                                            (conj result factor))
+      :else                          (recur rst
+                                            remaining
+                                            result))))
