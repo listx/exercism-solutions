ccdaa7442f3733b30794e3dca6ac13816c71e4bb
Linus Arver <linus@ucla.edu>
2024-06-09 20:31:01 +1000

    tweak kindergarten-garden
    Avoid the complicated row transposition logic, and instead just treat
    each row as a subproblem, and combine the results with `merge-with`.

--- clojure/kindergarten-garden/src/kindergarten_garden.clj
+++ clojure/kindergarten-garden/src/kindergarten_garden.clj
@@ -1,34 +1,33 @@
 (ns kindergarten-garden
   (:require [clojure.string :as str]))
 
-;; Transpose plants string into groups of 4 plants, with columnar ordering (not
-;; the row-wise ordering as given in the input).
+;; Create a list containing 2 items, one for each row.
 (defn parse-plants [plants]
-  (->> ;; => "ABCD\nEFGH"
+  (->> ;; => "CGRV\nCGRV"
        plants
 
-       ;; => ["ABCD" "EFGH"]
+       ;; => ["CGRV" "CGRV"]
        str/split-lines
 
-       ;; Note that this is a list of 2 lists now, one for each row.
-       ;;
-       ;; => (((\A \B) (\C \D)) ((\E \F) (\G \H)))
-       (map #(partition 2 %))
-
-       ;; We get the nth item from each of the 2 lists. The computed argument
-       ;; for `nth` is the column number (where column 0 gets (\A \B) from row 1
-       ;; and (\E \F) from row 2).
-       ;;
-       ;; => ((\A \B \E \F) (\C \D \G \H))
-       ((fn [rows]
-          (for [col (range (quot (dec (count plants)) 4))]
-            (mapcat #(nth % col) rows))))
-
        ;; Convert the letters into plant names.
-       (map #(replace {\C :clover
-                       \G :grass
-                       \R :radishes
-                       \V :violets} %))))
+       (map #(map {\C :clover
+                   \G :grass
+                   \R :radishes
+                   \V :violets} %))
+
+       ;; Note that this is a list of 2 lists now, one for each row. The key is
+       ;; that the plants are grouped into pairs. The key idea here is to assign
+       ;; one row correctly to each student, and to repeat this procedure again
+       ;; for the second row. Then we can just merge the two results together to
+       ;; get the combined answer where each student is responsible for both
+       ;; rows.
+       ;;
+       ;; => (((:clover :grass) (:radishes :violets)) ((:clover :grass) (:radishes :violets)))
+       (map #(partition 2 %))))
+
+(defn normalize-students [students]
+  (->> (sort students)
+       (map (comp keyword str/lower-case))))
 
 (defn garden
   ([plants] (garden plants
@@ -45,7 +44,6 @@
                      "Kincaid"
                      "Larry"]))
   ([plants students]
-   (->> students
-        (map (comp keyword str/lower-case))
-        sort
-        (#(zipmap % (parse-plants plants))))))
+   (->> (parse-plants plants)
+        (map #(zipmap (normalize-students students) %))
+        (apply merge-with concat))))
