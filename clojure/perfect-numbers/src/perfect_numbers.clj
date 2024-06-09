b35d31386a60e75b71fec27cf5b067609c14e417
Linus Arver <linus@ucla.edu>
2024-06-09 21:27:55 +1000

    solve perfect-numbers

--- clojure/perfect-numbers/src/perfect_numbers.clj
+++ clojure/perfect-numbers/src/perfect_numbers.clj
@@ -1,5 +1,13 @@
 (ns perfect-numbers)
 
-(defn classify [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn aliquot-sum [n]
+  (->> (range 1 n)
+       (filter #(zero? (mod n %)))
+       (apply +)))
+
+(defn classify [n]
+  (when (neg? n) (throw (IllegalArgumentException. "invalid input")))
+  (case (compare (aliquot-sum n) n)
+    -1 :deficient
+    0  :perfect
+       :abundant))
