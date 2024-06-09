5ab58d95aaf4fbe0e8ebffc1e870c520c1cf6335
Linus Arver <linus@ucla.edu>
2024-06-09 10:57:16 +1000

    tweak hexadecimal
    Use `Character/digit` to easily convert from the letter to its
    equivalent number.

--- clojure/hexadecimal/src/hexadecimal.clj
+++ clojure/hexadecimal/src/hexadecimal.clj
@@ -1,30 +1,12 @@
 (ns hexadecimal)
 
 (defn finalize [terms]
-  (if (some #(= :invalid-char %) terms)
+  (if (some neg? terms)
     0
     (apply + terms)))
 
 (defn hex-to-int [s]
   (->> (reverse s)
-       (map vector (iterate #(* 16 %) 1) )
-       (map (fn [[coeff ch]]
-              (case ch
-                \0 (* coeff  0)
-                \1 (* coeff  1)
-                \2 (* coeff  2)
-                \3 (* coeff  3)
-                \4 (* coeff  4)
-                \5 (* coeff  5)
-                \6 (* coeff  6)
-                \7 (* coeff  7)
-                \8 (* coeff  8)
-                \9 (* coeff  9)
-                \a (* coeff 10)
-                \b (* coeff 11)
-                \c (* coeff 12)
-                \d (* coeff 13)
-                \e (* coeff 14)
-                \f (* coeff 15)
-                :invalid-char)))
+       (map #(Character/digit % 16))
+       (map * (iterate #(* 16 %) 1))
        finalize))
