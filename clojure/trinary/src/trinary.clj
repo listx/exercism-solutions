98f632443d23dc9a80d46f3aec593ee95aeed7b2
Linus Arver <linus@ucla.edu>
2024-06-09 18:05:19 +1000

    solve trinary

--- clojure/trinary/src/trinary.clj
+++ clojure/trinary/src/trinary.clj
@@ -1,5 +1,16 @@
 (ns trinary)
 
-(defn to-decimal [] ;; <- arglist goes here
-      ;; your code goes here
-)
+;; Adapted from
+;; https://exercism.org/tracks/clojure/exercises/hexadecimal/solutions/listx.
+;; The only thing we need to change is the '16' base to just '3'.
+
+(defn finalize [terms]
+  (if (some neg? terms)
+    0
+    (apply + terms)))
+
+(defn to-decimal [s]
+  (->> (reverse s)
+       (map #(Character/digit % 3))
+       (map * (iterate #(* 3 %) 1))
+       finalize))
