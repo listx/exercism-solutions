33f6fc50453842b48d92814553c461857c2323be
Linus Arver <linus@ucla.edu>
2024-06-09 17:35:06 +1000

    tweak isbn-verifier

--- clojure/isbn-verifier/src/isbn_verifier.clj
+++ clojure/isbn-verifier/src/isbn_verifier.clj
@@ -1,7 +1,21 @@
 (ns isbn-verifier
   (:require [clojure.string :as str]))
 
-(defn helper [chars]
+(defn verify [chars]
+  (as-> chars x
+    ;; Convert characters to numbers. The last "X", if it's there, will
+    ;; become -1.
+    (map #(Character/digit % 10) x)
+    ;; Convert any -1 into 10, because that's what the "X" char means.
+    (map #(if (neg? %) 10 %) x)
+    ;; Convert to addends, sum them up, mod by 11, and check if the result
+    ;; is 0.
+    (map * (range 10 0 -1) x)
+    (apply + x)
+    (mod x 11)
+    (zero? x)))
+
+(defn validate-and-verify [chars]
   (cond
     ;; Exactly 10 digits found?
     (not= (count chars) 10) false
@@ -10,20 +24,8 @@
     ;; For the last char, is it a digit or "X"?
     (not (or (Character/isDigit (nth chars 9))
              (= (nth chars 9) \X))) false
-    :else
-      (as-> chars x
-        ;; Convert characters to numbers. The last "X", if it's there, will
-        ;; become -1.
-        (map #(Character/digit % 10) x)
-        ;; Convert any -1 into 10, because that's what the "X" char means.
-        (map #(if (neg? %) 10 %) x)
-        ;; Convert to addends, sum them up, mod by 11, and check if the result
-        ;; is 0.
-        (map * (range 10 0 -1) x)
-        (apply + x)
-        (mod x 11)
-        (zero? x))))
+    :else (verify chars)))
 
 (defn isbn? [isbn]
   (->> (str/replace isbn "-" "")
-       helper))
+       validate-and-verify))
