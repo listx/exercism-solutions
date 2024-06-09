d1466adfa796699f12b52f071723b829bf1ba5ba
Linus Arver <linus@ucla.edu>
2024-06-09 20:55:40 +1000

    tweak pascals-triangle

--- clojure/pascals-triangle/src/pascals_triangle.clj
+++ clojure/pascals-triangle/src/pascals_triangle.clj
@@ -5,17 +5,19 @@
 (def triangle
   (iterate
     (fn [above]          ; 1 3 3 1
-      ;; mapv is like Haskell's zipWith, but which supports arbitrary numbers of
-      ;; collections. And +' is an arbitrary-precision version of +.
-      (mapv +'
+
+      ;; mapv is like Haskell's zipWith.
+      (mapv +
 
         ;; Offset the preceding row with a zero at the end.
         (conj above 0)   ; 1 3 3 1 0
 
         ;; Offset the preceding row with a zero at the beginning.
         (cons 0 above))) ; 0 1 3 3 1
-    ;; First row is [1].
-    [1]))                ; 1 4 6 4 1 <-- This is what we get when we add the
+
+    ;; First row is [1N] (the N suffix makes the 1 an arbitrary-precision
+    ;; integer).
+    [1N]))               ; 1 4 6 4 1 <-- This is what we get when we add the
                          ;               columns together.
 
 (defn row [n]
