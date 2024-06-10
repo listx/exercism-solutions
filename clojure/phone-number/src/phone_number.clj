986ea5a9ec9385982da8d5a069695700a1bcc072
Linus Arver <linus@ucla.edu>
2024-06-10 10:57:02 +1000

    solve phone-number

--- clojure/phone-number/src/phone_number.clj
+++ clojure/phone-number/src/phone_number.clj
@@ -1,6 +1,20 @@
 (ns phone-number)
 
 (defn number
-  [] ;; <- arglist goes here
-  ;; your code goes here
-  )
+  [s]
+  (let [normalized (->> s
+                        (filter #(Character/isDigit %))
+                        (#(cond->> %
+                            (= 10 (count %)) (cons \1))))
+        country-code  (nth normalized 0)
+        area-code     (nth normalized 1)
+        exchange-code (nth normalized 4)
+        invalid "0000000000"]
+    (cond
+      (not= 11 (count normalized)) invalid
+      (not= \1 country-code)       invalid
+      (#{\0 \1} area-code)         invalid
+      (#{\0 \1} exchange-code)     invalid
+      :else (->> normalized
+                 (drop 1)
+                 (apply str)))))
