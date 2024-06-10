96c821ff6cd83b055c2e5981f41c065471aa2b97
Linus Arver <linus@ucla.edu>
2024-06-10 16:40:36 +1000

    tweak difference-of-squares
    Use closed form solutions for the sum of squares and the sum of the
    first N natural numbers. These formulas are well-known in mathematics.

--- clojure/difference-of-squares/src/difference_of_squares.clj
+++ clojure/difference-of-squares/src/difference_of_squares.clj
@@ -1,13 +1,10 @@
 (ns difference-of-squares)
 
 (defn sum-of-squares [n]
-  (->> (range 1 (inc n))
-       (map #(* % %))
-       (apply +)))
+  (quot (* n (inc n) (inc (* 2 n))) 6))
 
 (defn square-of-sum [n]
-  (->> (range 1 (inc n))
-       (apply +)
+  (->> (quot (* n (inc n)) 2)
        (#(* % %))))
 
 (defn difference [n]
