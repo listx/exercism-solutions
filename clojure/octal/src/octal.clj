e1ec80fc833c80dd9044c86f0454dcf2438f60a5
Linus Arver <linus@ucla.edu>
2024-06-10 16:45:42 +1000

    solve octal

--- clojure/octal/src/octal.clj
+++ clojure/octal/src/octal.clj
@@ -1,5 +1,16 @@
 (ns octal)
 
-(defn to-decimal [] ;; <- arglist goes here
-  ;; your code goes here
-)
+;; Adapted from
+;; https://exercism.org/tracks/clojure/exercises/hexadecimal/solutions/listx.
+;; The only thing we need to change is the '16' base to just '8'.
+
+(defn finalize [terms]
+  (if (some neg? terms)
+    0
+    (apply + terms)))
+
+(defn to-decimal [s]
+  (->> (reverse s)
+       (map #(Character/digit % 8))
+       (map * (iterate #(* 8 %) 1))
+       finalize))
