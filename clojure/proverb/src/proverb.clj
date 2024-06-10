e19df8ac95e0f3cf1cf83fcbc82c2bb6de58da40
Linus Arver <linus@ucla.edu>
2024-06-10 11:52:50 +1000

    tweak proverb
    See https://exercism.org/tracks/clojure/exercises/proverb/solutions/amirci.
    
    Some useful tricks learned:
    
    - Use (nil? fst) to check if the given collection was empty. This is
      equivalent to (empty? words) but is shorter and has symmetry with (nil?
      rst) which checks if there was only 1 element in the collection.
    
    - Use mapv to return a vector. This is simpler than using (map ...)
      followed by (apply vector).

--- clojure/proverb/src/proverb.clj
+++ clojure/proverb/src/proverb.clj
@@ -13,17 +13,19 @@
 ;; For want of a battle the kingdom was lost.
 ;; And all for the want of a nail.
 
+(defn leading-verse [[a b]]
+  (format "For want of a %s the %s was lost." a b))
+
+(defn last-verse [w]
+  (format "And all for the want of a %s." w))
+
 (defn recite
-  [[fst & _ :as words]]
-  (if (empty? words)
-    ""
-    (let [mk-leading-verse (fn [[a b]]
-                             (format "For want of a %s the %s was lost." a b))
-          leading-verses (->> words
-                              (partition 2 1)
-                              (map mk-leading-verse)
-                              (apply vector))
-          last-verse (format "And all for the want of a %s." fst)]
-      (->> leading-verses
-           (#(conj % last-verse))
-           (str/join "\n")))))
+  [[fst & rst :as words]]
+  (cond
+    (nil? fst) ""
+    (nil? rst) (last-verse fst)
+    :else (->> words
+               (partition 2 1)
+               (mapv leading-verse)
+               (#(conj % (last-verse fst)))
+               (str/join "\n"))))
