de07b083245c5e25a6724e5bc30f3dc7f51aec31
Linus Arver <linus@ucla.edu>
2024-06-10 15:57:28 +1000

    solve crypto-square

--- clojure/crypto-square/src/crypto_square.clj
+++ clojure/crypto-square/src/crypto_square.clj
@@ -1,21 +1,44 @@
-(ns crypto-square)
+(ns crypto-square
+  (:require [clojure.string :as str]))
 
-(defn normalize-plaintext [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn normalize-plaintext [s]
+  (->> (str/lower-case s)
+       (filter #(Character/isLetterOrDigit %))
+       (apply str)))
 
-(defn square-size [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn rect-size [s]
+  (let [len (count s)]
+    (->> (for [r (range 1 len) c (range 1 len)
+               :when (and (<= r c)
+                          (<= (- c r) 1)
+                          (<= len (* r c)))]
+           [r c])
+         first)))
 
-(defn plaintext-segments [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn square-size [s]
+  (->> (rect-size s)
+       second))
 
-(defn ciphertext [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn plaintext-segments [s]
+  (->> (normalize-plaintext s)
+       (#(partition-all (square-size %) %))
+       (mapv (partial apply str))))
 
-(defn normalize-ciphertext [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn pad-with-trailing-spaces [s n]
+  (take n (concat s (repeat \space))))
+
+(defn ciphertext-rect [s]
+  (->> (plaintext-segments s)
+       (#(conj (into '[] (butlast %))
+               (pad-with-trailing-spaces (last %)
+                                         (count (first %)))))
+       (apply map str)))
+
+(defn ciphertext [s]
+  (->> (ciphertext-rect s)
+       (apply str)
+       (#(str/replace % " " ""))))
+
+(defn normalize-ciphertext [s]
+  (->> (ciphertext-rect s)
+       (str/join " ")))
