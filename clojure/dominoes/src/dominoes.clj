eff57cc39a7e81d28a996b88c53e48be46c7ed26
Linus Arver <linus@ucla.edu>
2024-06-23 14:27:08 +1000

    dominoes: improve comments and naming

--- clojure/dominoes/src/dominoes.clj
+++ clojure/dominoes/src/dominoes.clj
@@ -1,6 +1,6 @@
 (ns dominoes)
 
-(defn chains [n ds]
+(defn grow-chains [dots ds]
   (for [
         ;; Iterate through all indices in ds. This way we can check all dominoes
         ;; in `ds` to be candidates for growing the chain (and not just the
@@ -9,37 +9,37 @@
         :let [
               ;; `l` is the left part of the partition from `split-at`. We don't
               ;; really care about `l` because we don't modify it. We only use
-              ;; `split-at` to try to find a domino that will "fit", which we
-              ;; can eliminate from the search by removing it. That's why it's
-              ;; missing from the `concat` call below.
+              ;; `split-at` to try to find a domino (`[a b]`) that will "fit",
+              ;; which we can eliminate from the search by removing it. That's
+              ;; why `[a b]` is missing from the `concat` call below.
               ;;
               ;; `[a b]` is the leftmost domino from the right part of the
               ;; partition from `split-at`.
               [l [[a b] & r]] (split-at i ds)
-              ;; `m` is the matching number on the leftmost domino from the
-              ;; chain-to-be-grown, which is the same as `n` (the right edge of
-              ;; the existing chain we have so far).
-              m (cond (= a n) b
-                      (= b n) a)]
+              ;; For [a b], if either a or b matches `dots`, then
+              ;; `dots-complement` is b or a, respectively (the *other* side of
+              ;; the matching domino).
+              dots-complement (cond (= a dots) b
+                                    (= b dots) a)]
         ;; Only consider growths where there is a match.
-        :when m]
+        :when dots-complement]
     ;; For the grown chain, leave out [a b] because we "selected" it to grow the
     ;; chain.
-    [m (concat l r)]))
+    [dots-complement (concat l r)]))
 
 (defn chain-end [b ds]
   ;; Use `some` to stop the search as soon as we find a valid chain that works.
-  (some (fn [[b ds]]
+  (some (fn [[dots ds]]
           ;; Consider only "full" chains that have finished growing.
           (if (empty? ds)
-            ;; The [b ds] means that `chains` will return a collection of pairs
-            ;; where `b` is the right edge of the chain. So return `b` if `ds`
-            ;; (dominoes we can use to grow the chain) is empty.
-            b
+            ;; The [dots ds] means that `grow-chains` will return a collection
+            ;; of pairs where `dots` is the right edge of the chain. So return
+            ;; `dots` if `ds` (dominoes we can use to grow the chain) is empty.
+            dots
             ;; If the chain being considered is not fully grown, recurse to grow
-            ;; it more (by calling `chains`).
-            (chain-end b ds)))
-        (chains b ds)))
+            ;; it more (by calling `grow-chains`).
+            (chain-end dots ds)))
+        (grow-chains b ds)))
 
 (defn can-chain? [[[a b] & ds]]
   (cond
@@ -47,9 +47,9 @@
     (nil? a) true
     ;; If only 1 domino, then it must have identical numbers on each side.
     (nil? ds) (= a b)
-    ;; If multiple dominoes, the left side of this first domino must equal the
-    ;; end of the chain (rightmost edge). The question then is building up a
+    ;; If multiple dominoes, the left side of this first domino (`a`) must equal
+    ;; the end of the chain (rightmost edge). The question then is building up a
     ;; valid chain such that we can use up all the dominoes. We need `b` to
     ;; start the chain, and `ds` to select the dominoes in the chain.
     :else (= a
-             (chain-end b (vec ds)))))
+             (chain-end b ds))))
