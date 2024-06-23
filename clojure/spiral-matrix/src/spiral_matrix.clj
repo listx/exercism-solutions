b9113b2d1d4dfcd6a9a4375408eaba8ee239e98b
Linus Arver <linus@ucla.edu>
2024-06-23 21:55:00 +1000

    tweak spiral-matrix
    Instead of iterating (infinitely) when we are working with finite data,
    just recurse until there's no more work to be done and terminate.
    
    Also prever `list` over `into ()` because the former preserves order (we
    don't need to use `reverse` any more).

--- clojure/spiral-matrix/src/spiral_matrix.clj
+++ clojure/spiral-matrix/src/spiral_matrix.clj
@@ -7,13 +7,10 @@
   (try (aget m y x)
        (catch ArrayIndexOutOfBoundsException _ -1)))
 
-(defn next-cell
-  "Return the next cell to visit in order to walk the given matrix as a spiral.
-  Use the starting position [y x] as a reference. Also, look around the matrix
-  itself to see which positions have not been walked yet, in order to determine
-  which direction we should use."
+(defn walk-spiral
+  "Walk a spiral around a matrix until we run out of room (until we're done)."
   [[m [y x]]]
-  (let [val   (aget m y x)
+  (let [old-val (aget m y x)
         north [(dec y) x]
         south [(inc y) x]
         east  [y (inc x)]
@@ -21,8 +18,13 @@
         at-north (at-pos north m)
         at-south (at-pos south m)
         at-east  (at-pos east m)
-        at-west  (at-pos west m)
-        [y-new x-new :as dir]
+        at-west  (at-pos west m)]
+    ;; If we've finished walking (no adjacent cell is unvisited), return the
+    ;; matrix as is.
+    (if (every? #(not (zero? %)) [at-north at-south at-east at-west])
+      m
+      ;; Otherwise recurse to mutate it, building up our spiral.
+      (let [[y-new x-new :as dir]
           (cond
             (and (zero? at-east)
                  ;; Only go east if the cell above us is iether out of bounds
@@ -32,12 +34,12 @@
             (zero? at-south) south
             (zero? at-west) west
             :else north)]
-    ;; `aset` does not return the 2d array. It returns the value we used. So
-    ;; here we invoke it to cause a side effect (the mutation of `m`).
-    (aset m y-new x-new (inc val))
-    ;; Now that `m` is mutated, return it along with the direction `dir` we
-    ;; used.
-    [m dir]))
+        ;; `aset` does not return the 2d array. It returns the value we used. So
+        ;; here we invoke it to cause a side effect (the mutation of `m`).
+        (aset m y-new x-new (inc old-val))
+        ;; Now that `m` is mutated, return it along with the direction `dir` we
+        ;; used.
+        (walk-spiral [m dir])))))
 
 (defn spiral [n]
   (case n
@@ -46,16 +48,9 @@
                  to-array-2d)]
       ;; Set top-left corner to 1.
       (aset m 0 0 1)
-      (->> (iterate next-cell [m [0 0]])
-           (drop (dec (* n n)))
-           first
-           first
+      (->> (walk-spiral [m [0 0]])
            ;; Visually this looks correct at this point like [[...]] (probably
            ;; because of our use of `to-array-2d`), but the tests require lists,
-           ;; not vectors, so we have to convert it as such. Because list
-           ;; construction reverses thinsgs, we have to un-reverse them back by
-           ;; manually by calling `reverse` on both the y and x axes.
-           (map #(into () %))
-           (map reverse)
-           (into ())
-           reverse))))
+           ;; not vectors, so we have to convert it as such.
+           (map #(apply list %))
+           (apply list)))))
