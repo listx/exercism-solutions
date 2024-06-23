07d61e0a4406a53b2ffc35fd86f1670fd18790eb
Linus Arver <linus@ucla.edu>
2024-06-23 14:55:37 +1000

    solve largest-series-product

--- clojure/largest-series-product/src/largest_series_product.clj
+++ clojure/largest-series-product/src/largest_series_product.clj
@@ -1,5 +1,20 @@
 (ns largest-series-product)
 
-(defn largest-product [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(def char-to-digit
+  {\0 0N
+   \1 1N
+   \2 2N
+   \3 3N
+   \4 4N
+   \5 5N
+   \6 6N
+   \7 7N
+   \8 8N
+   \9 9N})
+
+(defn largest-product [n digits]
+  (let [groups (partition n 1 digits)
+        products (->> groups
+                      (map #(map char-to-digit %))
+                      (map #(apply * %)))]
+  (apply max products)))
