df87f25c8b25f525695a46092273b7c215d59102
Linus Arver <linus@ucla.edu>
2024-06-30 14:00:28 +1000

    solve luhn

--- clojure/luhn/src/luhn.clj
+++ clojure/luhn/src/luhn.clj
@@ -1,5 +1,23 @@
 (ns luhn)
 
-(defn valid? [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn valid? [s]
+  (let [stripped (->> s
+                      (filter #(Character/isDigit %))
+                      (apply str))]
+    (cond
+      ;; If invalid characters, invalid.
+      (some false? (map #(or (Character/isSpaceChar %)
+                             (Character/isDigit %)) s)) false
+      ;; If too short, invalid.
+      (< (count stripped) 2) false
+      :else (->> stripped
+                 (map #(Character/digit % 10))
+                 reverse
+                 (map vector (cycle [1 2]))
+                 (map (partial apply *))
+                 (map #(if (< 9 %)
+                         (- % 9)
+                         %))
+                 (apply +)
+                 (#(rem % 10))
+                 zero?))))
